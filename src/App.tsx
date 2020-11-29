fdd4166f613b8d69138b090812a9adeb697d0693
André <andrevictor50@gmail.com>
2020-11-29 18:06:36 -0300

    adding urql, graphql and yup

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import 'react-native-gesture-handler';
 
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { Provider, createClient } from 'urql';
 import AuthRoutes from './routes';
+
+const client = createClient({ url: 'http://localhost:3333' });
 
 const App: React.FC = () => {
   return (
-    <NavigationContainer>
-      <AuthRoutes />
-    </NavigationContainer>
+    <Provider value={client}>
+      <NavigationContainer>
+        <AuthRoutes />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
