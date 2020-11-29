ea0e6428fc13fb0d696a9fedb87f58c5c1d85fb0
André <andrevictor50@gmail.com>
2020-11-29 14:46:29 -0300

    success page completed

--- src/pages/SuccessPage/index.tsx
+++ src/pages/SuccessPage/index.tsx
@@ -26,24 +26,69 @@ interface ISuccessPageProps {
 const SuccessPage: React.FC<ISuccessPageProps> = ({ route }) => {
   const { CreateAccount, UpdatedProfile, SuccessOperation } = route.params;
 
-  return (
-    <Container>
-      <Background source={UnionBackground} />
-      <Content>
-        <SuccessIconContainer>
-          <Icon name="check" color="#03B352" size={45} />
-        </SuccessIconContainer>
-        <ActionTitle>Conta Criada!</ActionTitle>
-        <ActionDescription>
-          Agora é só fazer login
-          {'\n'} e aproveitar.
-        </ActionDescription>
-      </Content>
-      <Button>
-        <ButtonText>Ok</ButtonText>
-      </Button>
-    </Container>
-  );
+  if (CreateAccount) {
+    return (
+      <Container>
+        <Background source={UnionBackground} />
+        <Content>
+          <SuccessIconContainer>
+            <Icon name="check" color="#03B352" size={45} />
+          </SuccessIconContainer>
+          <ActionTitle>Conta Criada!</ActionTitle>
+          <ActionDescription>
+            Agora é só fazer login {'\n'} e aproveitar.
+          </ActionDescription>
+        </Content>
+        <Button>
+          <ButtonText>Ok</ButtonText>
+        </Button>
+      </Container>
+    );
+  }
+
+  if (UpdatedProfile) {
+    return (
+      <Container>
+        <Background source={UnionBackground} />
+        <Content>
+          <SuccessIconContainer>
+            <Icon name="check" color="#03B352" size={45} />
+          </SuccessIconContainer>
+          <ActionTitle>Feito!</ActionTitle>
+          <ActionDescription>
+            Agora suas informações {'\n'} estão atualizadas.
+          </ActionDescription>
+        </Content>
+        <Button>
+          <ButtonText>Ok</ButtonText>
+        </Button>
+      </Container>
+    );
+  }
+
+  if (SuccessOperation) {
+    return (
+      <Container>
+        <Background source={UnionBackground} />
+        <Content>
+          <SuccessIconContainer>
+            <Icon name="check" color="#03B352" size={45} />
+          </SuccessIconContainer>
+          <ActionTitle>Carro Alugado!</ActionTitle>
+          <ActionDescription>
+            Agora você só precisa ir {'\n'}
+            até a concessionária da RENTX {'\n'}
+            para pegar o seu automóvel.
+          </ActionDescription>
+        </Content>
+        <Button>
+          <ButtonText>Ok</ButtonText>
+        </Button>
+      </Container>
+    );
+  }
+
+  return <Container />;
 };
 
 export default SuccessPage;
