646974b774f621e280727645842f29c6b6b7d322
André <andrevictor50@gmail.com>
2020-11-30 12:35:50 -0300

    redux store

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,22 @@ import 'react-native-gesture-handler';
 
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { Provider, createClient } from 'urql';
-import AuthRoutes from './routes';
+import { Provider as URQLProvider, createClient } from 'urql';
+import { Provider as ReduxProvider } from 'react-redux';
+import Routes from './routes';
+import store from './store';
 
 const client = createClient({ url: 'http://localhost:3333/graphql' });
 
 const App: React.FC = () => {
   return (
-    <Provider value={client}>
-      <NavigationContainer>
-        <AuthRoutes />
-      </NavigationContainer>
-    </Provider>
+    <ReduxProvider store={store}>
+      <URQLProvider value={client}>
+        <NavigationContainer>
+          <Routes />
+        </NavigationContainer>
+      </URQLProvider>
+    </ReduxProvider>
   );
 };
 
